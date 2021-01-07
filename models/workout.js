37e62ae0d468359d9ef410891dfe04d00505a545
Garrett Walter <gtwalter150@gmail.com>
2021-01-07 14:44:06 -0500

    created mongoose schema/model

---
+++ models/workout.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: 
+    [{
+        type: {type: String, required: "Enter exercise type"},
+        name: {type: String, required: "Enter exercise name"},
+        duration: {type: Number},
+        weight: {type: Number},
+        reps: {type: Number},
+        sets: {type: Number}
+    }],
+});
+
+const Workout = mongoose.model("Workout", workoutsSchema);
+
+module.exports = Workout;
