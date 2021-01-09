3e25a19831e1785b8f9be5fbb5e0d82bc5f74da3
Garrett Walter <gtwalter150@gmail.com>
2021-01-09 01:42:23 -0500

    created html routes for stats and exercise

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+var path = require("path");
 
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
@@ -30,6 +31,14 @@ router.post("/api/workouts", ({ body }, res) => {
       .catch(err => {
         res.status(400).json(err);
       });
+  });
+
+  router.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 
 
